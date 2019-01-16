d9d0319935c3ecce882e3b0047358e1b79218526
vv <316193839@qq.com>
2019-01-16 16:42:13 +0800

    modify the tpl css

--- models/models.go
+++ models/models.go
@@ -1,8 +1,6 @@
 package models
 
 import (
-	"fmt"
-
 	"github.com/astaxie/beego/orm"
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -31,9 +29,9 @@ func TotalNum() int64 {
 	o := orm.NewOrm()
 	o.Using("std")
 
-	std := new(Student)                   // 默认使用 std，你可以指定为其他数据库
-	cnt, err := o.QueryTable(std).Count() // SELECT COUNT(*) FROM STUDENT
-	fmt.Printf("std Count Num: %s, %s", cnt, err)
+	std := new(Student)                 // 默认使用 std，你可以指定为其他数据库
+	cnt, _ := o.QueryTable(std).Count() // SELECT COUNT(*) FROM STUDENT
+	//fmt.Printf("std Count Num: %s, %s", cnt, err)
 	return cnt
 }
 
@@ -42,7 +40,7 @@ func HasHanle() int64 {
 	o.Using("std")
 
 	std := new(Student)
-	cnt, err := o.QueryTable(std).Filter("is_handle", 1).Count() // SELECT COUNT(*) FROM STUDENT WHERE IS_HANDLE = 1
-	fmt.Printf("std Count Num: %s, %s", cnt, err)
+	cnt, _ := o.QueryTable(std).Filter("is_handle", 1).Count() // SELECT COUNT(*) FROM STUDENT WHERE IS_HANDLE = 1
+	//fmt.Printf("std Count Num: %s, %s", cnt, err)
 	return cnt
 }
--- views/index.tpl
+++ views/index.tpl
@@ -51,7 +51,8 @@
   <footer>
     <div class="author">
       Official website:
-      <a href="http://{{.Website}}">{{.Website}}</a> /
+      <a href="http://{{.Website}}">{{.Website}}</a> 
+	<br>
       Contact me:
       <a class="email" href="mailto:{{.Email}}">{{.Email}}</a>
     </div>
