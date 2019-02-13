1cf34441070d9708a76d96d6f6a6d9f0f855b63c
wilson <316193839@qq.com>
2019-02-13 11:18:19 +0800

    fixed the  filter

--- main.go
+++ main.go
@@ -5,10 +5,18 @@ import (
 	"student/views"
 
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context"
 )
 
-func main() {
+var FilterUser = func(ctx *context.Context) {
+	_, ok := ctx.Input.Session("uid").(int)
+	if !ok && ctx.Request.RequestURI != "/login" {
+		ctx.Redirect(302, "/login")
+	}
+}
 
+func main() {
+	beego.InsertFilter("/*", beego.BeforeRouter, FilterUser)
 	//beego.BConfig.WebConfig.Session.SessionOn = true
 	beego.AddFuncMap("add", views.Add)
 	beego.AddFuncMap("div", views.Div)
--- routers/router.go
+++ routers/router.go
@@ -1,21 +1,12 @@
 package routers
 
 import (
-	"context"
 	"student/controllers"
 
 	"github.com/astaxie/beego"
 )
 
-var FilterUser = func(ctx *context.Context) {
-	_, ok := ctx.Input.Session("uid").(int)
-	if !ok && ctx.Request.RequestURI != "/login" {
-		ctx.Redirect(302, "/login")
-	}
-}
-
 func init() {
-	beego.InsertFilter("/*", beego.BeforeRouter, FilterUser)
 	beego.Router("/", &controllers.MainController{})
 	beego.Router("/login", &controllers.LoginController{})
 }
